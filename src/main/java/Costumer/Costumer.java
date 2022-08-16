391c2f8e2fbf0bbaa2d97f7066754519d93fcb27
rishavemail786 <rishavemail786@gmail.com>
2022-08-16 15:03:15 +0530

    working it

--- src/main/java/Costumer/Costumer.java
+++ src/main/java/Costumer/Costumer.java
@@ -4,7 +4,7 @@ public class Costumer {
     private String name;
     private String address;
     private String email;
-    private int    mobileNumber;
+    private long mobileNumber;
     private boolean premiumMemberShip;
 
     public Costumer(String name, String address, String email, int mobileNumber, boolean premiumMemberShip) {
@@ -13,17 +13,68 @@ public class Costumer {
         this.email = email;
         this.mobileNumber = mobileNumber;
         this.premiumMemberShip = premiumMemberShip;
+        System.out.println("costumer name = " + this.name + "address=  " + address + "email =" + email + "mobile number" + " =" + mobileNumber + "premium member ship =" +premiumMemberShip);
     }
 
     public Costumer() {
-        name ="";
+        name = "";
         address = "";
         email = "";
-        mobileNumber =0;
+        mobileNumber = 0;
         premiumMemberShip = false;
-        System.out.println("costumer name = "+this.name+"address=  "+address+"email ="+email+"mobile number" +
-                " ="+mobileNumber+"premium member ship ="+ false);
+        System.out.println("costumer name = " + this.name + "address=  " + address + "email =" + email + "mobile number" + " =" + mobileNumber + "premium member ship =" + premiumMemberShip);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public boolean isPremiumMemberShip() {
+        return premiumMemberShip;
+    }
+
+    public void setPremiumMemberShip(boolean premiumMemberShip) {
+        this.premiumMemberShip = premiumMemberShip;
+    }
+
+    public void displayCostumer() {
+        System.out.println("costumer name =" + name);
+        System.out.println("costumer address =" + address);
+        System.out.println("costumer email =" + email);
+        System.out.println("costumer mobile number =" + mobileNumber);
+        System.out.println("costumer premium member ship =" + premiumMemberShip);
 
 
     }
+
+
 }
--- src/main/java/main/Costumer/CostumerImpl.java
+++ src/main/java/main/Costumer/CostumerImpl.java
@@ -1,7 +1,33 @@
 package main.Costumer;
 
+import Costumer.Costumer;
+
 public class CostumerImpl {
     public static void main(String[] args) {
+        Costumer object1 =new Costumer("abc","xyz","abc@gmail.com",12345,true);
+        object1.displayCostumer();
+        Costumer object2 = new Costumer();
+        object2.displayCostumer();
+       /* object2.setName("rishav");
+        object2.setAddress("begusarai");
+        object2.setEmail("rishavemail786@gmail.com");
+        object2.setMobileNumber((long) 6_299_614_762l);
+        object2.setPremiumMemberShip(true);
+        System.out.println(object1.getName());
+        System.out.println(object1.getEmail());
+        System.out.println(object1.getAddress());
+        System.out.println(object1.getMobileNumber());
+        System.out.println(object1.isPremiumMemberShip());*/
+
+
+
+
+
+
+
+
+
+
 
 
     }
